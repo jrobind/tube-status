b0b275c415d74f2cca606285c827daae3053d856
jrobind <jrobind@outlook.com>
2019-06-25 21:16:08 +0100

    add success and failure scenarios when handling network requests

--- sw.js
+++ sw.js
@@ -2,9 +2,7 @@
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
         navigator.serviceWorker.register('./sw.js')
-            .then((reg) => {
-                console.log('Service worker registered.', reg);
-            })
+            .then((reg) => console.log('Service worker registered.', reg));
     });
 }
 
@@ -20,7 +18,8 @@ self.addEventListener("install", (event) => {
     console.log('WORKER: install event in progress.');
 
     event.waitUntil(
-        caches.open(version + 'fundamentals')   
+        caches
+            .open(version + 'fundamentals')   
             .then((cache) => cache.addAll(offlineFundamentals))
             .then(() => console.log('WORKER: install completed'))
     );
@@ -32,14 +31,42 @@ self.addEventListener("fetch", (event) => {
     if (event.request.method !== 'GET') return;
 
     event.respondWith(
-        caches.match(event.request)
-        .then((cached) => {
-            const networked = fetch(event.request)
-                .then(fetchedFromNetwork, unableToResolve)
-                .catch(unableToResolve);
-            // return the cached response immediately if there is one, othwerwise wait on network 
-            return cached || networked;
-        })
+        caches
+            .match(event.request)
+            .then((cached) => {
+                const networked = fetch(event.request)
+                    .then(fetchedFromNetwork, unableToResolve)
+                    .catch(unableToResolve);
+                    // return the cached response immediately if there is one, othwerwise wait on network 
+                return cached || networked;
+
+                function fetchedFromNetwork(response) {
+                    const cacheCopy = response.clone();
+                    console.log('WORKER: fetch response from network.', event.request.url);
+
+                    caches
+                        .open(version + 'pages')
+                        .then((cache) => {
+                            return cache.put(event.request, cacheCopy);
+                        })
+                        .then(() => console.log('WORKER: fetch response stored in cache.', event.request.url));
+                        
+                    return response;
+                }
+       
+                // provide a meaningful response even when all else fails
+                function unableToResolve () {
+                    console.log('WORKER: fetch request failed in both cache and network.');
+          
+                    return new Response('<h1>Service Unavailable</h1>', {
+                        status: 503,
+                        statusText: 'Service Unavailable',
+                        headers: new Headers({
+                            'Content-Type': 'text/html'
+                        })
+                    });
+                }
+            })
     );
 });
 
