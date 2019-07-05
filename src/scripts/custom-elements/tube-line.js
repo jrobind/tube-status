62b13bf4b00f30e0832e9e67c4f175da61e892e9
jrobind <jrobind@outlook.com>
2019-07-05 21:55:23 +0100

    updates to tube-line and tube-status-wrapper custom elements

--- src/scripts/custom-elements/tube-line.js
+++ src/scripts/custom-elements/tube-line.js
@@ -1,4 +1,4 @@
-import { store, getLine } from '../utils/store.js';
+import { subscribeToStore, updateStore, getStore } from '../utils/store.js';
 
 export default class TubeLine extends HTMLElement {
     constructor() {
@@ -10,14 +10,46 @@ export default class TubeLine extends HTMLElement {
     }
 
     connectedCallback() {
+        // subscribe to store updatesn (order matters)
+        subscribeToStore([this.processLineStatus_.bind(this), this.updateDOM_.bind(this)]);
         this.tubeStatusWrapper_  = document.querySelector('tube-status-wrapper');
-        this.lineStatus_ = getLine(this.line_);
+        this.render_();
+    }
+
+    processLineStatus_() {
+        console.log('update triggered in process line status')
+        for (let [key, value] of Object.entries(getStore().lineData.lines)) {
+            if (value.id === this.line_) this.lineStatus_ = value;
+        }
+
+        console.log(this.lineStatus_)
+    }
+
+    updateDOM_() {
+        const status = this.lineStatus_.lineStatuses[0].statusSeverityDescription
+        this.shadowRoot.querySelector('#line-status').innerText = `${this.line_}: ${status}`;
     }
 
     attributeChangedCallback(name, oldValue, newValue) {
     }
 
     render_() {
-        // render markup
+        const template = document.createElement('template');
+        template.innerHTML = `
+            <style>
+            .line-wrapper {
+                background: red;
+                width: 100%;
+                height: 60px;
+            }
+            </style>
+            <div class="line-wrapper" current-status>
+                <div id="line-status"></div>
+                <div>SUBSCRIBE TO ALERTS</div>
+            </div>
+        `;
+        const templateContent = template.content;
+        const shadowRoot = this.attachShadow({mode: 'open'})
+            .appendChild(templateContent.cloneNode(true));
     }
 }
--- src/scripts/custom-elements/tube-status-wrapper.js
+++ src/scripts/custom-elements/tube-status-wrapper.js
@@ -1,28 +1,53 @@
-import { store, updateStore } from '../utils/store.js';
+import { subscribeToStore, updateStore, getStore } from '../utils/store.js';
 import { fetchAllLineStatus } from '../utils/api.js';
 
+/** @type {number} */
+const FETCH_INTERVAL = 30000;
+
+/**
+ * Tube status wrapper custom element.
+ */
 export default class TubeStatusWrapper extends HTMLElement {
     constructor() {
         super();
-        
+
+        /** @private {object} */
         this.results_ = null;
     }
 
-    async connectedCallback() {
+    connectedCallback() {
+        this.getAllLineData_();
+        // get data every 30 seconds
+        this.initialise_();
+    }
+    
+    /**
+     * Handles the fetching of all API tube data.
+     * @private
+     */
+    async getAllLineData_() {
         try {
             this.results_ = await fetchAllLineStatus();
-            this.updateStore_(this.results_);
+            // update store with successful API response
+            updateStore(this.results_);
         } catch(e) {
             this.handleError_(e);
         } 
     }
 
-    updateStore_(results) {
-        // update store
-        updateStore(results);
+    /**
+     * Sets up the API fetch interval process.
+     * @private
+     */
+    initialise_()  {
+        setInterval(() => {
+            this.getAllLineData_();
+        }, FETCH_INTERVAL);
     }
 
+    /** @private */
     handleError_(e) {
         console.error(e);
+        alert('Unable to retrieve API data.');
     }
 }
