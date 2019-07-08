79a40e0a5a5adc30a9e789453b8e16e9ffcf85eb
jrobind <jrobind@outlook.com>
2019-07-08 17:37:31 +0100

    add live updates for dlr & overground services

--- src/scripts/utils/api.js
+++ src/scripts/utils/api.js
@@ -1,11 +1,26 @@
 
 export const fetchLineStatus = async (line) => {
     const url = `https://api.tfl.gov.uk/Line/${line}/Disruption?app_id=e1ff8d90&app_key=%2040e8038774d4f1324c35203ac2a4f0c2`;
+    let template = { name: null, id: null, lineStatuses: null };
+
+    switch(line)  {
+        case 'tfl-rail': 
+            template = { name: 'Tfl Rail', id:  'tfl-rail', lineStatuses: null };
+            break;
+        case 'london-overground': 
+            template = { name: 'Overground', id:  'london-overground', lineStatuses: null };
+            break;
+        case 'dlr': 
+            template = { name: 'dlr', id:  'dlr', lineStatuses: null };
+    }
 
     try {
         const response = await fetch(url);
         const deserialisedJSON = await response.json();
-        return deserialisedJSON;
+
+        template.lineStatuses = deserialisedJSON.length ? deserialisedJSON : [{ statusSeverityDescription: 'Good Service' }];
+
+        return template;
     } catch(e) {
         throw new Error(e);
     }
@@ -13,18 +28,12 @@ export const fetchLineStatus = async (line) => {
 
 export const fetchAllLineStatus = async () => {
     const url = `https://api.tfl.gov.uk/Line/Mode/tube/Status?app_id=e1ff8d90&app_key=%2040e8038774d4f1324c35203ac2a4f0c2`;
-    const rail =  { name: 'TfL Rail', id:  'tfl-rail', lineStatuses: null };
 
     try {
-        const responseAll = await fetch(url);
-        const railResponse = await fetchLineStatus('tfl-rail');
+        const [ all, rail, overground, dlr] = await Promise.all([fetch(url), fetchLineStatus('tfl-rail'), fetchLineStatus('london-overground'), fetchLineStatus('dlr')]);
+        const deserialisedJSON = await all.json(); 
 
-        const deserialisedJSON = await responseAll.json(); 
-
-        rail.lineStatuses = railResponse.length ? railResponse : [{ statusSeverityDescription: 'Good Service' }];
-        deserialisedJSON.push(rail);
-
-        return deserialisedJSON;
+        return deserialisedJSON.concat([rail, overground, dlr]);
     } catch(e) {
         throw new Error(e);
     }
