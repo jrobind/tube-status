a365fc36b935933f194a6aa5a7060cb24f9cf4df
jrobind <jrobind@outlook.com>
2019-07-09 20:28:37 +0100

    update html to include 'slot' attribute and make associated changes in tube-line custom element

--- src/scripts/custom-elements/tube-line.js
+++ src/scripts/custom-elements/tube-line.js
@@ -1,21 +1,31 @@
 import { subscribeToStore, updateStore, getStore } from '../utils/store.js';
 
+/**
+ * Tube line custom element.
+ */
 export default class TubeLine extends HTMLElement {
     constructor() {
         super();
 
+        /** @private {string} */
         this.line_ = this.getAttribute('line');
+        /** @private {HTMLElement} */
         this.tubeStatusWrapper_ = null;
+        /** @private {object} */
         this.lineStatus_ = null;
     }
 
     connectedCallback() {
-        // subscribe to store updatesn (order matters)
+        // subscription order matters. DOM updates depend on the processing of line status.
         subscribeToStore([this.processLineStatus_.bind(this), this.updateDOM_.bind(this)]);
-        this.tubeStatusWrapper_  = document.querySelector('tube-status-wrapper');
+        this.tubeStatusWrapper_ = document.querySelector('tube-status-wrapper');
         this.render_();
     }
 
+    /**
+     * Sets current line status data for custom element.
+     * @private
+     */
     processLineStatus_() {
         console.log('update triggered in process line status')
         for (let [key, value] of Object.entries(getStore().lineData.lines)) {
@@ -25,14 +35,23 @@ export default class TubeLine extends HTMLElement {
         console.log(this.lineStatus_)
     }
 
+    /**
+     * Updates DOM with every new line status update.
+     * @private
+     */
     updateDOM_() {
-        const status = this.lineStatus_.lineStatuses[0].statusSeverityDescription
-        this.shadowRoot.querySelector('#line-status').innerText = `${this.line_}: ${status}`;
+        // need to update this to handle multiple disruptions
+        let status = this.lineStatus_.lineStatuses[0].statusSeverityDescription;
+
+        if (!status) status = this.lineStatus_.lineStatuses[0].closureText;
+        
+        this.querySelector('.tube-line-status').innerText = `${this.line_}: ${status}`;
     }
 
-    attributeChangedCallback(name, oldValue, newValue) {
-    }
-
+    /**
+     * Renders custom element shadow DOM. Invoked each time the custom element is appended to DOM. 
+     * @private
+     */
     render_() {
         const template = document.createElement('template');
         template.innerHTML = `
@@ -44,12 +63,12 @@ export default class TubeLine extends HTMLElement {
             }
             </style>
             <div class="line-wrapper" current-status>
-                <div id="line-status"></div>
+                <slot name="line-status"></slot>
                 <div>SUBSCRIBE TO ALERTS</div>
             </div>
         `;
         const templateContent = template.content;
-        const shadowRoot = this.attachShadow({mode: 'open'})
+        this.attachShadow({mode: 'open'})
             .appendChild(templateContent.cloneNode(true));
     }
 }
