a4536d7743d707e2fc367b60ac35d0c27168eb97
jrobind <jrobind@outlook.com>
2019-07-10 21:21:54 +0100

    move files

--- src/scripts/index.js
+++
@@ -1,7 +0,0 @@
-import tubeStatusWrapper from './custom-elements/tube-status-wrapper.js';
-import tubeLine from './custom-elements/tube-line.js';
-
-window.customElements.define('tube-status-wrapper', tubeStatusWrapper);
-window.customElements.define('tube-line', tubeLine);
-
-
