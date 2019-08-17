b170c5e8e7e03455d07b9ae9a0680e63791485be
jrobind <jrobind@outlook.com>
2019-08-17 17:03:04 +0100

    update User model schema

--- server/models/User.js
+++ server/models/User.js
@@ -3,10 +3,11 @@ require('dotenv').config();
 
 // User schema
 const userSchema = new mongoose.Schema({
+    googleId: String,
     name: String,
     email: String,
-    lines: Array,
-    avatar: String
+    avatar: String,
+    lines: Array
 });
 
 const User = mongoose.model('User', userSchema);
