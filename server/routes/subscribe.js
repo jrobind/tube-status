237d7c6d21d8788ab1a6ffba00a9b51b8b468553
jrobind <jrobind@outlook.com>
2019-08-17 17:18:58 +0100

    user added to db using google profile data

--- server/routes/subscribe.js
+++ server/routes/subscribe.js
@@ -1,18 +1,55 @@
-const express = require("express");
-const db = require('./models');
+const express = require('express');
+const fetch = require('node-fetch');
+const db = require('../models');
 
 const router = express.Router();
 
 // subscribe Route
 router.post('/subscribe', (req, res) => {
-    console.log('REACHING SUBSCRIBE')
-    const { pushSubscription, line } = req.body;
-    dataStore.lines.push(line);
+    const { token } = req.body;
+    const lines = req.body.line || [];
+    console.log(req.body)
+    fetch(`https://oauth2.googleapis.com/tokeninfo?id_token=${token}`)
+        .then(res => {
+            console.log(res.status)
+            // 200 response means token is validated
+            if (res.status === 200) {
+                return res.json()
+            }
+        })
+        .then(data => {
+            // check aud claim matches client ID
+            if (data.aud === '55387840936-mejpra12qf0d4rlrr6n3kta1boor8jh1.apps.googleusercontent.com') {
+                const googleId = data.sub;
+                const profileData = { 
+                    googleId, // store user in db using unique Google id
+                    name: data.name,
+                    email: data.email,
+                    avatar: data.picture,
+                    lines
+                };
+                    // check if user exists. If not, then add to db.
+                    db.UserModel.findOne({ googleId }, (err, resp) => {
+                        if (resp) {
+                            console.log('User already exists');
+                            return
+                        } else {
+                            const newUser = new db.UserModel(profileData);
+                            newUser.save()
+                                .then((newUser) => console.log('User added to db', newUser));
+                        }
+                    })
+            }
+        })
+        .catch(err => console.log(err))
+    
+    // const { pushSubscription, line } = req.body;
+    // dataStore.lines.push(line);
 
-    const payload = JSON.stringify({title: "TESTING"})
-    webpush
-        .sendNotification(pushSubscription, payload)
-        .catch(err => console.error(err));
+    // const payload = JSON.stringify({title: "TESTING"})
+    // webpush
+    //     .sendNotification(pushSubscription, payload)
+    //     .catch(err => console.error(err));
 
 });
 
