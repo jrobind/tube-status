206f29d0977b51966f52137606d07ec65c299da6
jrobind <jrobind@outlook.com>
2019-09-08 19:59:11 +0100

    add loading custom element

---
+++ client/src/scripts/custom-elements/loading.js
@@ -0,0 +1,40 @@
+import { subscribeToStore, updateStore, getStore } from '../utils/store.js';
+
+/**
+ * Loading custom element.
+ */
+export default class Loading extends HTMLElement {
+    constructor() {
+        super();
+
+        this.appWrapper_ = document.querySelector('.app-wrapper');
+    }
+
+    connectedCallback() {
+        subscribeToStore(this.handleLoading_.bind(this));
+    } 
+
+    /**
+     * Handles loading logic. 
+     * @private
+     */
+    handleLoading_() {
+        if (getStore().loadingState) {
+            this.appWrapper_.setAttribute('hide', '');
+            this.removeAttribute('hide');
+            this.setAttribute('show', '');
+        } else {
+            this.appWrapper_.removeAttribute('hide');
+            this.removeAttribute('show');
+            this.setAttribute('hide', '');
+        }
+    }
+
+    /**
+     * Renders link authentication text. 
+     * @private
+     */
+    render_() {
+        this.innerHTML = this.authenticationText_;
+    }
+}
