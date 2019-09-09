52eb5aeddc309cb2b81caf6bb8d1c2bd0735b408
jrobind <jrobind@outlook.com>
2019-09-09 21:01:17 +0100

    use switch statement and refactor store properties

--- client/src/scripts/utils/store.js
+++ client/src/scripts/utils/store.js
@@ -6,29 +6,33 @@ let store = {
     userProfile: {
         signedIn: false,
         id: null,
-        displayName: null,
         avatar: null,
-        email: null
     },
+    loadingState: false,
     subscribers: []
 }
 
 export const getStore = () => store;
 
-export const updateStoreAuth = (data) => {
-    const newStore = { ...store, userProfile:{ ...data } };
-    store = newStore;
-    // invoke subscribers
-    // if (store.subscribers.length) store.subscribers.forEach(callback => callback());
-    // return store;
-}
+export const updateStore = (action, data) => {
+    switch(action) {
+        case 'LINES':
+            store = { ...store, lineData:{ lines: {  ...data } } };
+            // invoke subscribers
+            store.subscribers.length && store.subscribers.forEach(callback => callback());
 
-export const updateStore = (data) => {
-    const newStore = { ...store, lineData:{ lines: {  ...data } } };
-    store = newStore;
+            return store;
+        case 'AUTH':
+            store = { ...store, userProfile:{ ...data } };
+            store.subscribers.length && store.subscribers.forEach(callback => callback());
 
-    // if (store.subscribers.length) store.subscribers.forEach(callback => callback());
-    // return store;
+            return store;
+        case 'LOADING':
+            store = { ...store, ...data };
+            store.subscribers.length && store.subscribers.forEach(callback => callback());
+
+            return store;
+    }
 }
 
 export const subscribeToStore = (callbacks) => {
