caa7b093c70a2e861b310c382fd14509caa0c5ac
jrobind <jrobind@outlook.com>
2019-09-11 19:41:38 +0100

    define new custom elements

--- client/src/scripts/index.js
+++ client/src/scripts/index.js
@@ -1,7 +1,9 @@
 import tubeStatusWrapper from './custom-elements/tube-status-wrapper.js';
 import tubeLine from './custom-elements/tube-line.js';
+import authentication from './custom-elements/authentication.js';
+import loading from './custom-elements/loading.js';
 
+window.customElements.define('tube-status-loading', loading);
 window.customElements.define('tube-status-wrapper', tubeStatusWrapper);
 window.customElements.define('tube-line', tubeLine);
-
-
+window.customElements.define('tube-status-authentication', authentication);
