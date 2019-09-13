3eeeefd64f5a01c3f1482f8e90e00a7b77c9cfdc
jrobind <jrobind@outlook.com>
2019-09-13 16:30:05 +0100

    add ability to handle subscription requests & dynamically render markup based on store updates

--- client/src/scripts/custom-elements/authentication.js
+++ client/src/scripts/custom-elements/authentication.js
@@ -10,43 +10,82 @@ export default class Authentication extends HTMLElement {
         /** @private {string} */
         this.dest_ = this.getAttribute('dest');  
         /** @private {string} */
-        this.authPath_ = this.getAttribute('auth-path');  
+        this.authPath_ = this.getAttribute('auth-path');
+        /** @private {string} */       
+        this.authenticationText_ = this.authPath_ === 'lineAuth' ? 'sign-in to subscribe' : 'Log in with Google';
     }
 
     connectedCallback() {
+        // fetch('/protected', {
+        //     headers: { 'Authorization':  `Bearer ${localStorage.getItem('JWT'))}`}
+        // }).then(res => console.log(res))
+        subscribeToStore(this.attemptUpdate_.bind(this), this.handleSubscriptionRequest.bind(this));
+        this.render_();
         this.addEventListener('click', this.handleAuth_.bind(this));
     } 
 
-    // attributeChangedCallback(attrName, oldVal, newVal) {
-    //     if (attrName  === 'auth')
-    // }
+    /**
+     * Attempts attribute & authentication text updates after authentication. 
+     * @private
+     */
+    attemptUpdate_() {
+        if (getStore().userProfile.signedIn) {
+            if (this.authPath_ !== 'lineAuth') {
+                this.setAttribute('auth-path', 'logout');
+                this.authPath_ = this.getAttribute('auth-path');
+            }
+            this.authenticationText_ = this.authPath_ !== 'lineAuth' ? this.authPath_ : 'subscribe';
+            this.render_();
+        }
+    }
 
     /**
      * Handle Google authentication process.
      * @private
      */
     handleAuth_() {
+        const { userProfile } = getStore();
+
         switch(this.authPath_) {
             case 'login':
-                debugger;
                 window.location.href = this.dest_;
-                this.setAttribute('auth-path', 'logout');
                 break;
             case 'logout':
                 localStorage.removeItem('JWT');
                 updateStore('AUTH', { signedIn: false, displayName: null, email: null, avatar: null, id: null });
                 window.location.href = '/';
-                this.setAttribute('auth-copy', 'login');
                 break;
             case 'lineAuth':
-                // post subscription
-                fetch('/subscribe',{ 
-                    method: 'POST',
-                    body: JSON.stringify({ pushSubscription }),
-                    headers: {
-                        'content-type': 'application/json'
-                    }
-                });
+                userProfile.signedIn ? this.handleSubscriptionRequest() : window.location.href = this.dest_;
+                break;
         }
+    }
+
+     /**
+     * Handles line subscription request. 
+     * @private
+     */
+    handleSubscriptionRequest() {
+        const { userProfile, pushSubscription } = getStore();
+        const line = this.parentElement.parentElement.getAttribute('line');
+
+        if (userProfile.signedIn && pushSubscription) {
+            fetch('api/subscribe',{ 
+                method: 'POST',
+                body: JSON.stringify({ pushSubscription, line }),
+                headers: {
+                    'content-type': 'application/json',
+                    'Authorization': `Bearer ${localStorage.getItem('JWT')}`
+                }
+            });
+        }
+    }
+
+    /**
+     * Renders link authentication text. 
+     * @private
+     */
+    render_() {
+        this.innerHTML = this.authenticationText_;
     }
 }
