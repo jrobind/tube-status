0976c01858e0cff24674b28a85b7e8440abb88fe
jrobind <jrobind@outlook.com>
2019-09-26 20:39:04 +0100

    add jwt middleware and save push subscription object with every line sub

--- server/routes/subscribe.js
+++ server/routes/subscribe.js
@@ -1,62 +1,53 @@
 const express = require('express');
-const jwt = require('jsonwebtoken');
 const passport = require('passport'); 
 const db = require('../models');
+const middleware = require('../middleware');
 
 const router = express.Router();
 
 // get user's line subscriptions
-router.get('/subscribe', passport.authenticate('jwt', { session: false }), (req, res) => {
-    jwt.verify(req.headers.authorization.split(' ')[1], 'secret', (err,  decoded) => {
-        if (err) console.log('cannot verify jwt');
-        const googleId = decoded._json.sub;
-        // find current user line subscriptions and send to client
-        db.UserModel.findOne({ googleId }, (err, resp) => {
-            if (resp) {
-                res.json({ lines: resp.lines });
-            } else {
-                console.log('not finding user')
-            }
-        });
+router.get('/subscribe', passport.authenticate('jwt', { session: false }), middleware.jwtVerify, (req, res) => {
+    const googleId = res.decoded._json.sub;
+    // find current user line subscriptions and send to client
+    db.UserModel.findOne({ googleId }, (err, resp) => {
+        if (resp) {
+            res.json({ lines: resp.lines });
+        } else {
+            console.log('not finding user')
+        }
     });
 });
 
-// make a new line subscription
-router.post('/subscribe', passport.authenticate('jwt', { session: false }), (req, res) => {
-    jwt.verify(req.headers.authorization.split(' ')[1], 'secret', (err,  decoded) => {
-        if (err) console.log('cannot verify jwt');
-        const googleId = decoded._json.sub;
-        const lines = req.body.line || [];
-
-        db.UserModel.findOneAndUpdate({ googleId }, { $push: { lines }},
-            (err, success) => {
-                if (err) {
-                    console.log('Failed to update');
-                } else {
-                    console.log('Successfully updated', success);
-                    res.json({ lines });
-                }
-        });
+// create a new line subscription
+router.post('/subscribe', passport.authenticate('jwt', { session: false }), middleware.jwtVerify, (req, res) => {
+    const googleId = res.decoded._json.sub;
+    const lines = req.body.line || [];
+    const pushSubscription = req.body.pushSubscription;
+    // save new line subscription and push subscription object to user
+    db.UserModel.findOneAndUpdate({ googleId }, { $push: { lines }, $set: { pushSubscription } },
+        (err, success) => {
+            if (err) {
+                console.log('Failed to update');
+            } else {
+                console.log('Successfully updated', success);
+                res.json({ lines });
+            }
     });
 });
 
 // remove line subscription
-router.delete('/subscribe', passport.authenticate('jwt', { session: false }), (req, res) => {
-    console.log('REACHING')
-    jwt.verify(req.headers.authorization.split(' ')[1], 'secret', (err,  decoded) => {
-        if (err) console.log('cannot verify jwt');
-        const googleId = decoded._json.sub;
-        const line = req.body.line;
+router.delete('/subscribe', passport.authenticate('jwt', { session: false }), middleware.jwtVerify, (req, res) => {
+    const googleId = res.decoded._json.sub;
+    const line = req.body.line;
 
-        db.UserModel.findOneAndUpdate({ googleId }, { $pullAll: { lines: [line] }},
-            (err, success) => {
-                if (err) {
-                    console.log('Failed to update');
-                } else {
-                    console.log('Successfully removed line', success);
-                    res.json({ lines: line });
-                }
-        });
+    db.UserModel.findOneAndUpdate({ googleId }, { $pullAll: { lines: [line] }},
+        (err, success) => {
+            if (err) {
+                console.log('Failed to update');
+            } else {
+                console.log('Successfully removed line', success);
+                res.json({ lines: line });
+            }
     });
 });
 
