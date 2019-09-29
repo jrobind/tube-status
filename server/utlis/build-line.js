203560d78f5eea8d7c63d65f58b1004e0e8da91c
jrobind <jrobind@outlook.com>
2019-09-29 16:30:56 +0100

    update build-line function

--- server/utlis/build-line.js
+++ server/utlis/build-line.js
@@ -1,21 +1,27 @@
 const db = require('../models');
 const fetch = require("node-fetch");
 
-module.exports = () => {
-    db.LinesModel.find({}, async (err, resp) => {
-        const response = await fetch('http://localhost:4500/api/lines').catch(e => console.log(e));
-        const result = await response.json();
-        const lineData = {};
-    
-        // build db line data
-        result.forEach(line => {
-            const status = line.lineStatuses[0].statusSeverityDescription === 'Good Service';
-            const description = line.lineStatuses[0].statusSeverityDescription || line.lineStatuses[0].closureText;
-            lineData[line.id] = { goodService: status, description };
-        });
-    
-        const newLineData = new db.LinesModel(lineData);
-        newLineData.save()
-            .then((lines) => console.log('Line data added to db', lines)); 
+module.exports = async () => {
+    const url = 'http://localhost:4500/api/lines'; 
+    const response = await fetch(url).catch(e => console.log(e));
+    const result = await response.json();
+    const lineData = {};
+
+    // build db line data
+    result.forEach(line => {
+        const status = line.lineStatuses[0].statusSeverityDescription === 'Good Service';
+        const description = line.lineStatuses[0].statusSeverityDescription || line.lineStatuses[0].closureText;
+        lineData[line.id] = { goodService: status, description };
+    }); 
+
+    db.LinesModel.findOneAndUpdate({}, lineData, (err, resp) => {
+        if (!resp) {
+            // first line db entry
+            const newLineData = new db.LinesModel(lineData);
+            newLineData.save()
+                .then((lines) => console.log('First line data added to db', lines));
+        } else {
+            console.log('line data updated in db from build-line', resp);
+        }
     });
 }
