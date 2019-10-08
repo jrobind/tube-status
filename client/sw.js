ffa6f3a82f737e2724fbd0fede0ee9020ef81ed1
jrobind <jrobind@outlook.com>
2019-10-08 19:19:22 +0100

    update service worker

--- client/sw.js
+++ client/sw.js
@@ -1,70 +1,44 @@
-// use async/await once implemented
-const version = 'v1::';
-
-const offlineFundamentals = [
-    './src/scripts/index.js',
-    './images/train-large.png'
+const version = 'v1-tube-status';
+const contentToCache = [
+	'./index.html',
+	'./offline.html',
+	'./src/styles/app.css',
+	'./src/scripts/index.js',
+	'./push-setup.js',
+	'./images/train-large.png',
+	'./images/train-medium.png',
+	'./images/train-small.png',
+	'./images/train-splash-screen.png'
 ];
 
-self.addEventListener('push', e => {
-    const data =  e.data.json();
-    console.log('push recieved', data)
-    self.registration.showNotification(data.title, {
-        body: "Notified by Tube Status",
-    });
+self.addEventListener('push', (e) => {
+		const { line, status } =  e.data.json();
+    self.registration.showNotification(`${line} line`, { body: status });
 });
 
-self.addEventListener("install", (event) => {
-    event.waitUntil(
-        caches
-            .open(version + 'fundamentals')   
-            .then((cache) => cache.addAll(offlineFundamentals))
-    );
+self.addEventListener('install', (e) => {
+	e.waitUntil(async function() {
+		const cacheOpen = await caches.open(version);
+		return await cacheOpen.addAll(contentToCache)
+	}());
 });
 
-self.addEventListener("fetch", (event) => {
-    // console.log('WORKER: fetch event in progress.');
-    // only cache a GET request
-    if (event.request.method !== 'GET') return;
-
-    event.respondWith(
-        caches
-            .match(event.request)
-            .then((cached) => {
-                const networked = fetch(event.request)
-                    .then(fetchedFromNetwork)
-                    // .catch(() => console.log('WORKER: fetch request failed in both cache and network.'));
-                // return the cached response immediately if there is one, othwerwise wait on network 
-                return cached || networked;
-
-                function fetchedFromNetwork(response) {
-                    const cacheCopy = response.clone();
-                    // console.log('WORKER: fetch response from network.', event.request.url);
-
-                    caches
-                        .open(version + 'pages')
-                        .then((cache) => {
-                            return cache.put(event.request, cacheCopy);
-                        })
-                        // .then(() => console.log('WORKER: fetch response stored in cache.', event.request.url));
-                        
-                    return response;
-                }
-            })
-    );
+self.addEventListener('activate', (e) => {
+	e.waitUntil(async function() {
+		const keys = await caches.keys();
+		// clear out old cache resources
+		return await Promise.all(keys.map((key) => {
+			if (version.indexOf(key) === -1) return caches.delete(key);
+		}));
+	}());
 });
 
-self.addEventListener("activate", (event) => {
-    // console.log('WORKER: activate event in progress.');
-    event.waitUntil(
-        caches
-            .keys()
-            .then((keys) => {
-                return Promise.all(
-                    keys.filter((key) => !key.startsWith(version)).map((key) => caches.delete(key))
-                );    
-            })
-            // .then(() => console.log('WORKER: activate completed.'))
-    )
-});
+self.addEventListener('fetch', (e) => {
+	e.respondWith(async function() {
+		const cachedResponse = await caches.match(e.request);
 
+    if (cachedResponse) return cachedResponse;
+    // If fetch throws an exception then render custom offline page
+    return fetch(e.request).catch((e) => caches.match('./offline.html'));
+  }());
+});
