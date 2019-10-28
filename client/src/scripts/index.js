5247aa2dbccf771b4dd62423670c162d82bf1e5d
jrobind <jrobind@outlook.com>
2019-10-28 19:43:15 +0000

    define modal custom element globally

--- client/src/scripts/index.js
+++ client/src/scripts/index.js
@@ -2,7 +2,9 @@ import tubeStatusWrapper from './custom-elements/tube-status-wrapper.js';
 import tubeLine from './custom-elements/tube-line.js';
 import authentication from './custom-elements/authentication.js';
 import loading from './custom-elements/loading.js';
+import modal from './custom-elements/modal.js';
 
+window.customElements.define('tube-status-modal', modal);
 window.customElements.define('tube-status-loading', loading);
 window.customElements.define('tube-status-wrapper', tubeStatusWrapper);
 window.customElements.define('tube-line', tubeLine);
