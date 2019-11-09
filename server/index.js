79e56f23e2ecdc43afc6b8277d3175a3c25fd42a
jrobind <jrobind@outlook.com>
2019-11-09 16:57:07 +0000

    widescale sever changes

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require('path');
 const webpush = require("web-push");
 const fetch = require("node-fetch");
 const bodyParser = require("body-parser");
@@ -17,7 +18,7 @@ const line = require('./routes/lines');
 
 const app = express();
 
-app.use(express.static('../client'));
+app.use(express.static(path.join(__dirname, '../client')));
 app.use(bodyParser.json());
 db.mongoSetup();
 app.use(passport.initialize()); 
@@ -25,7 +26,7 @@ app.use('/api/', subscribe);
 app.use('/api/', line);
 
 // vapid keys
-const publicKey = 'BAyM-TL7OKAfqC9A9AkUnHqyzf3Cw3yEkFmvNCg56H6GjRMxUOyW-YK4_DJ_BdFRuSFB-lxJpqXjyxFVX_hdGe4';
+const publicKey = 'BKpELtYde7iajTdW7hw1LOIksmgzApC5IMLUtwDkqDA_fdqWmdQ3FqU2azo0LH0G-2cIqq11gRrxCLtFj-pPSmE';
 const privateKey = process.env.PRIVATE_KEY;
 
 webpush.setVapidDetails("mailto:test@test.com", publicKey, privateKey); 
@@ -47,8 +48,8 @@ passport.use(new JWTStrategy({
 passport.use(new GoogleStrategy({
       clientID: process.env.GOOGLE_AUTH_CLIENT_ID,
       clientSecret: process.env.GOOGLE_AUTH_CLIENT_SECRET,
-      callbackURL: 'http://localhost:4500/auth/google/callback',
-      scope: ['email', 'profile'],
+      callbackURL: 'http://localhost:4000/auth/google/callback',
+      scope: ['email', 'profile']
     },
     // verify function
     (accessToken, refreshToken, profile, cb) => {
@@ -57,31 +58,33 @@ passport.use(new GoogleStrategy({
     },
   ));
 
-app.get('/auth/google/callback', passport.authenticate('google', { failureRedirect: '/', session: false }), async (req, res) => {
-    console.log('wooo we authenticated, here is our user object:', req.user);
-    const googleId = req.user.id;
-    const profileData = { 
-        googleId, // store user in db using unique Google id
-        name: req.user.displayName,
-        email: req.user.emails[0].value,
-        avatar: req.user.photos[0].value,
-    };
-    // check if user exists. If not, then add to db.
-    await db.UserModel.findOne({ googleId }, (err, resp) => {
-        let lines;
-        if (!resp) {
-            const newUser = new db.UserModel(profileData);
-            newUser.save()
-                .then((newUser) => {
-                    console.log('User added to db', newUser);
-                    // add user line data to req.user so we can sign token send back to client
-                    lines = newUser.lines;
-                });
-        } else {
-            lines = resp.lines;
-        }
-        // add user line data to req.user so we can sign token send back to client
-        req.user.lines = lines;
+app.get('/auth/google/callback', 
+    passport.authenticate('google', { failureRedirect: '/', session: false, prompt: 'select_account' }),
+    async (req, res) => {
+        console.log('wooo we authenticated, here is our user object:', req.user);
+        const googleId = req.user.id;
+        const profileData = { 
+            googleId, // store user in db using unique Google id
+            name: req.user.displayName,
+            email: req.user.emails[0].value,
+            avatar: req.user.photos[0].value,
+        };
+        // check if user exists. If not, then add to db.
+        await db.UserModel.findOne({ googleId }, (err, resp) => {
+            let lines;
+            if (!resp) {
+                const newUser = new db.UserModel(profileData);
+                newUser.save()
+                    .then((newUser) => {
+                        console.log('User added to db', newUser);
+                        // add user line data to req.user so we can sign token send back to client
+                        lines = newUser.lines;
+                    });
+            } else {
+                lines = resp.lines;
+            }
+            // add user line data to req.user so we can sign token send back to client
+            req.user.lines = lines;
     });
 
     const htmlWithEmbeddedJWT = `
@@ -110,26 +113,31 @@ app.get('/protected', passport.authenticate('jwt', { session: false }), (req, re
 
 // run line status check every two minutes and send push notification to relevant line subscribers   
 setInterval(async () => {
-    const response = await fetch('http://localhost:4500/api/lines').catch(e => console.log(e));
+    const response = await fetch('http://localhost:4000/api/lines').catch(e => console.log(e));
     const result = await response.json();
     let lineDbData;
     // check if there is a status diff, query users with related line subscription and send notification
     await db.LinesModel.find({}, (err, resp) => lineDbData = resp);
 
     result.forEach(line => {
-        const id =  line.id;
-        const status = line.lineStatuses[0].statusSeverityDescription === 'Good Service';
-        const description = line.lineStatuses[0].statusSeverityDescription || line.lineStatuses[0].closureText;
+        const { statusSeverityDescription, reason } = line.lineStatuses[0];
+        const id = line.id;
+        const diffExists = lineDbData[0][id].description !== statusSeverityDescription;
 
-        if (lineDbData[0][id].goodService !== status) {
-            // query users and send notification, then update line data to reflect new status
+        if (true) {
+            // query users and send notification
+            // update line data to reflect new status
             db.UserModel.find({ 'lines': { $in: id } }, (err, resp) => {
                 if (resp.length) {
                     const { pushSubscription } = resp[0]; 
-                    const payload = JSON.stringify({ line: id, status: description });
+                    const payload = JSON.stringify({
+                        line: id,
+                        status: reason ? reason : statusSeverityDescription // if delays, send the reason. Otherwise send default description
+                     });
                     // send push notification
                     webpush
                         .sendNotification(pushSubscription, payload)
+                        .then(res => console.log(res))
                         .catch(err => console.error(err));
                 }
             });
@@ -139,4 +147,8 @@ setInterval(async () => {
     buildLine();
 }, 120000);
 
-app.listen(4500, () => console.log(`Server started on port 4500`));
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './client/index.html'));
+});
+
+app.listen(4000, () => console.log(`Server started on port 4000`));
