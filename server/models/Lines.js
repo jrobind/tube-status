be63a04214964f6e0616cd1a307dde298f34220b
jrobind <jrobind@outlook.com>
2019-11-10 18:38:31 +0000

    update Line schema

--- server/models/Lines.js
+++ server/models/Lines.js
@@ -4,59 +4,73 @@ const mongoose = require('mongoose');
 const LinesSchema = new mongoose.Schema({
     'bakerloo': {
         goodService: Boolean,
-        description: String
+        description: String,
+        reason: String
     },
     'central': {
         goodService: Boolean,
-        description: String
+        description: String,
+        reason: String
     },
     'circle': {
         goodService: Boolean,
-        description: String
+        description: String,
+        reason: String
     }, 
     'district': {
         goodService: Boolean,
-        description: String
+        description: String,
+        reason: String
     },
     'hammersmith-city': {
         goodService: Boolean,
-        description: String
+        description: String,
+        reason: String
     },
     'jubilee': {
         goodService: Boolean,
-        description: String
+        description: String,
+        reason: String
     },
     'metropolitan': {
         goodService: Boolean,
-        description: String
+        description: String,
+        reason: String
     },
     'northern': {
         goodService: Boolean,
-        description: String
+        description: String,
+        reason: String
     },
     'piccadilly': {
         goodService: Boolean,
-        description: String
+        description: String,
+        reason: String
     },
     'victoria': {
         goodService: Boolean,
-        description: String
+        description: String,
+        reason: String
     },
     'waterloo-city': {
         goodService: Boolean,
-        description: String
+        description: String,
+        reason: String
     },
     'tfl-rail': {
         goodService: Boolean,
-        description: String
+        description: String,
+        reason: String
     }, 
     'london-overground': {
         goodService: Boolean,
-        description: String
+        description: String,
+        reason: String
     },
-    dlr: {
+    'dlr': {
         goodService: Boolean,
-        description: String
+        description: String,
+        reason: String
     }
 });
 
