0722b48c2f4fd62aa5a5a75d97a35a878fe2f486
jrobind <jrobind@outlook.com>
2019-11-13 21:26:47 +0000

    populate modal with line disruption information

--- client/src/scripts/custom-elements/modal.js
+++ client/src/scripts/custom-elements/modal.js
@@ -19,24 +19,47 @@ export default class Modal extends HTMLElement {
     /**
      * Gets the relevant line information from client store. 
      * @private
+     * @param {string} line
      */
-    handleLineInformation_() {
+    populateModal_(line) {
+        const { reason } = getStore().lineInformation[line];
 
+        const caption = document.createElement('div');
+        caption.classList.add('tube-status-modal__caption');
+        caption.textContent = reason;
+
+        this.render_(caption);
     }
 
     /**
      * Shows modal with line information releavnt to clicked line. 
+     * @param {Object} e
      * @private
      */
-    toggleModal_() {
+    toggleModal_(e) {
         const isHidden = this.classList.contains('tube-status-modal--hidden');
+        const line = e.detail.line;
 
         if (isHidden) {
+            this.setAttribute('isHidden', 'false');
+
             this.classList.remove('tube-status-modal--hidden');
             this.classList.add('tube-status-modal--active');
+            this.populateModal_(line);
         } else {
+            this.setAttribute('isHidden', 'true');
+
             this.classList.remove('tube-status-modal--active');
             this.classList.add('tube-status-modal--hidden');
         }
+    }
+
+    /**
+     * Renders inner modal content depending on use case. 
+     * @param {Element} content
+     * @private
+     */
+    render_(content) {
+        this.appendChild(content);
     }
 }
