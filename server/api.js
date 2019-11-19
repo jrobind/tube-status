2fc42abe6c3cd0585c0a4e99e08ad52bfcf3024b
jrobind <jrobind@outlook.com>
2019-11-19 21:35:03 +0000

    add line fetch 30 second timeout

--- server/api.js
+++ server/api.js
@@ -1,17 +1,29 @@
 const fetch = require("node-fetch");
+const AbortController = require("abort-controller");
 require('dotenv').config({path: '../.env'});
 
+const timeout = 30000;
 const key = process.env.API_KEY;
+const url = `https://api.tfl.gov.uk/Line/Mode/tube,overground,dlr,tflrail/Status?app_id=${key}`;
+const controller = new AbortController();
+const signal = controller.signal;
+
+// 30 second timeout
+const timeoutId = setTimeout(() => controller.abort(), timeout);
 
 const fetchAllLineStatus = async () => {
-    const url = `https://api.tfl.gov.uk/Line/Mode/tube,overground,dlr,tflrail/Status?app_id=${key}`;
-
-    try {
-        const result = await fetch(url);
-        return await result.json();
-    } catch(e) {
-        throw new Error(e);
+  try {
+    const result = await fetch(url, { signal });
+    return await result.json();
+  } catch(e) {
+    if (e.name === 'AbortError') {
+      throw new Error('Fetch for line data timed out'); 
+    } else {
+      throw new Error(`Failed to retrieve line data: ${e}`); 
     }
+  } finally {
+    clearTimeout(timeoutId);
+  }
 }
 
 module.exports = { fetchAllLineStatus }
