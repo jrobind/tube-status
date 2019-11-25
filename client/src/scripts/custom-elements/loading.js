d419532818c06fff55580ae0172181700cc07127
jrobind <jrobind@outlook.com>
2019-11-25 15:33:54 +0000

    loading CE tidy up

--- client/src/scripts/custom-elements/loading.js
+++ client/src/scripts/custom-elements/loading.js
@@ -1,5 +1,16 @@
 import { subscribeToStore, updateStore, getStore } from '../utils/store.js';
 
+const cssClass = {
+  LOADING_ACTIVE: 'tube-status-loading--active',
+  LOADING_LINE_ACTIVE: 'tube-status-loading--active-line',
+  APP_HIDDEN: 'app-wrapper--hidden',
+  LINE_HIDDEN: 'tube-status-loading--hidden',
+};
+
+const cssSelector = {
+  APP_WRAPPER: '.app-wrapper',
+};
+
 /**
  * Loading custom element.
  */
@@ -7,7 +18,7 @@ export default class Loading extends HTMLElement {
   constructor() {
     super();
 
-    this.appWrapper_ = document.querySelector('.app-wrapper');
+    this.appWrapper_ = document.querySelector(cssSelector.APP_WRAPPER);
   }
 
   connectedCallback() {
@@ -27,7 +38,7 @@ export default class Loading extends HTMLElement {
   handleLoading_() {
     const { loadingState: { type,  state } } = getStore();
     const isLoadingTypeApp = type === 'app';
-    const activeClass = isLoadingTypeApp ? 'tube-status-loading--active' : 'tube-status-loading--active-line';
+    const activeClass = isLoadingTypeApp ? cssClass.LOADING_ACTIVE : cssClass.LOADING_LINE_ACTIVE;
     const isLoading = state;
 
     // exit if line loading context
@@ -35,13 +46,13 @@ export default class Loading extends HTMLElement {
     
     if (isLoading) {
       // app loading
-      isLoadingTypeApp && this.appWrapper_.classList.add('app-wrapper--hidden');
-      this.classList.remove('tube-status-loading--hidden');
+      isLoadingTypeApp && this.appWrapper_.classList.add(cssClass.APP_HIDDEN);
+      this.classList.remove(cssClass.LINE_HIDDEN);
       this.classList.add(activeClass);         
     } else {
-      isLoadingTypeApp && this.appWrapper_.classList.remove('app-wrapper--hidden');
+      isLoadingTypeApp && this.appWrapper_.classList.remove(cssClass.APP_HIDDEN);
       this.classList.remove(activeClass);  
-      this.classList.add('tube-status-loading--hidden');
+      this.classList.add(cssClass.LINE_HIDDEN);
     }
   }
 
