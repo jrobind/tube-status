d410b0e73e33ae45387522f84e53610d5f695d33
jrobind <jrobind@outlook.com>
2019-12-02 21:26:40 +0000

    header custom element now handles avatar logic

--- client/src/scripts/custom-elements/header.js
+++ client/src/scripts/custom-elements/header.js
@@ -1,15 +1,12 @@
 import { subscribeToStore, updateStore, getStore } from '../utils/store.js';
 
-const cssClass = {
-  LOADING_ACTIVE: 'tube-status-loading--active',
-  LOADING_LINE_ACTIVE: 'tube-status-loading--active-line',
-  APP_HIDDEN: 'app-wrapper--hidden',
-  LINE_HIDDEN: 'tube-status-loading--hidden',
+const cssSelector = {
+  HEADER_AUTHENTICATION: '.tube-status-header__authentication',
+  HEADER_AVATAR: '.tube-status-header__avatar-image',
 };
 
-const cssSelector = {
-  APP_WRAPPER: '.app-wrapper',
-};
+/** @const {string} */
+const AVATAR_IMG_PATH = '/images/account_circle.svg';
 
 /**
  * Header custom element.
@@ -18,17 +15,34 @@ export default class Header extends HTMLElement {
   constructor() {
     super();
 
+    /** @private {HTMLElement} */
+    this.avatarEl_;
+
+     /** @private {HTMLElement} */
+    this.authenticationEl_;
   }
 
   connectedCallback() {
-  
+    subscribeToStore({
+      callback: this.updateAvatar_.bind(this),
+      action: 'AUTH'
+    });
+    this.avatarEl_ = this.querySelector(cssSelector.HEADER_AVATAR);
+    this.authenticationEl_ = this.querySelector(cssSelector.HEADER_AUTHENTICATION);
+    this.updateAvatar_();
   }
 
   /**
-   * Renders link authentication text. 
+   * Updates avatar src attribute after login/logout. 
    * @private
    */
-  render_() {
-    this.innerHTML = this.authenticationText_;
+  updateAvatar_() {
+    const { userProfile: { signedIn, avatar } } = getStore();
+    if (signedIn) {
+      this.avatarEl_.src = avatar;
+    } else {
+      this.avatarEl_.src = AVATAR_IMG_PATH;
+    }
   }
+
 }
