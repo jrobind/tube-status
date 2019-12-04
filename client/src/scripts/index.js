b8065698b5d0292f0aef0e5675f0a65333fed41d
jrobind <jrobind@outlook.com>
2019-12-04 21:21:50 +0000

    define new custom element

--- client/src/scripts/index.js
+++ client/src/scripts/index.js
@@ -3,9 +3,11 @@ import tubeLine from './custom-elements/tube-line.js';
 import authentication from './custom-elements/authentication.js';
 import loading from './custom-elements/loading.js';
 import modal from './custom-elements/modal.js';
+import header from './custom-elements/header.js';
 
 window.customElements.define('tube-status-modal', modal);
 window.customElements.define('tube-status-loading', loading);
 window.customElements.define('tube-status-wrapper', tubeStatusWrapper);
 window.customElements.define('tube-line', tubeLine);
+window.customElements.define('tube-status-header', header);
 window.customElements.define('tube-status-authentication', authentication);
