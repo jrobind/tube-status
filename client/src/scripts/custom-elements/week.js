2be2ad850cbcc06f10075e09c9af430ea64e0fc5
jrobind <jrobind@outlook.com>
2019-12-29 19:01:05 +0000

    add basic render logic for week CE

--- client/src/scripts/custom-elements/week.js
+++ client/src/scripts/custom-elements/week.js
@@ -3,6 +3,15 @@ import {actions, delayTypes, customEvents} from "../constants.js";
 const {subscribeToStore, getStore} = store;
 
 /**
+ * CSS classes.
+ * @enum {string}
+ */
+const cssClass = {
+  DAY: "tube-status-week-day",
+  DAY_SELECT: "tube-status-week-day__select"
+};
+
+/**
  * Tube line custom element.
  */
 export default class Week extends HTMLElement {
@@ -16,7 +25,8 @@ export default class Week extends HTMLElement {
 
   /** Called every time element is inserted to DOM. */
   connectedCallback() {
-    this.render_();
+    document.addEventListener(
+      customEvents.SHOW_WEEK, this.render_.bind(this));
   }
 
   /**
@@ -38,6 +48,50 @@ export default class Week extends HTMLElement {
    * @private
    */
   render_() {
+    const table = document.createElement("table");
+    const tablehead = document.createElement("thead");
+    const tableBody = document.createElement("tbody");
+    const tableRow = document.createElement("tr");
+    const dayKey = {
+      0: "Monday",
+      1: "Tuesday",
+      2: "Wednesday",
+      3: "Thursday",
+      4: "Friday",
+      5: "Saturday",
+      6: "Sunday"
+    };
+    const days = Array.from(Array(7).keys()).map((n) => {
+      const day = document.createElement("th");
 
+      day.classList.add(cssClass.DAY);
+      day.innerText = dayKey[n];
+      
+      return day;
+    });
+
+    days.forEach((day) => {
+      const td = document.createElement("td");
+
+      td.classList.add(cssClass.DAY_SELECT);
+      td.setAttribute("day", day.innerText);
+
+      tablehead.appendChild(day);
+      tableRow.appendChild(td);
+    });
+  
+    table.appendChild(tablehead);
+    tableBody.appendChild(tableRow);
+    table.appendChild(tableBody);
+    this.appendChild(table);
+  }
+
+  /**
+   * Called each time custom element is disconnected from the DOM.
+   * @private
+   */
+  disconnectedCallback() {
+    document.removeEventListener(
+      customEvents.SHOW_WEEK, this.render_);
   }
 }
