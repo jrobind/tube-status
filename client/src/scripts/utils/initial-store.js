710b2e49a41bc15bc8160694cb4b8f7d10175af5
jrobind <jrobind@outlook.com>
2020-01-02 17:13:53 +0000

    update and tidy store structure

--- client/src/scripts/utils/initial-store.js
+++ client/src/scripts/utils/initial-store.js
@@ -3,21 +3,20 @@ export const initialStore = {
   lineData: {
     lines: {},
   },
-
   userProfile: {
     signedIn: false,
     id: null,
     avatar: null,
   },
-
   loadingState: {
     state: false,
     line: null,
   },
-
   pushSubscription: null,
-
   lineSubscriptions: [],
-
+  subscriptionData: {
+    days: null,
+    times: null,
+  },
   lineInformation: null,
 };
