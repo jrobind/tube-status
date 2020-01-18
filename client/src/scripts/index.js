9ae7fe32ecd354ee68bc1c4dc5a78fbd3aff635c
jrobind <jrobind@outlook.com>
2020-01-18 22:31:43 +0000

    define new custom elements

--- client/src/scripts/index.js
+++ client/src/scripts/index.js
@@ -5,6 +5,8 @@ import loading from "./custom-elements/loading.js";
 import modal from "./custom-elements/modal.js";
 import header from "./custom-elements/header.js";
 import week from "./custom-elements/week.js";
+import time from "./custom-elements/time.js";
+import tooltip from "./custom-elements/tooltip.js";
 
 window.customElements.define("tube-status-modal", modal);
 window.customElements.define("tube-status-loading", loading);
@@ -13,3 +15,5 @@ window.customElements.define("tube-line", tubeLine);
 window.customElements.define("tube-status-header", header);
 window.customElements.define("tube-status-authentication", authentication);
 window.customElements.define("tube-status-week", week);
+window.customElements.define("tube-status-time", time);
+window.customElements.define("tube-status-tooltip", tooltip);
