6efb63238c828552d0fdaffa7df8291b693402bb
jrobind <jrobind@outlook.com>
2020-01-18 22:32:48 +0000

    add new util helper function

--- client/src/scripts/utils/helpers.js
+++ client/src/scripts/utils/helpers.js
@@ -35,3 +35,30 @@ export const create = (elType, options = {}) => {
 
   return el;
 };
+
+export const returnKeys = (type) => {
+  const dayKey = ["Mo", "Tu", "We", "Th", "Fr", "Sa", "Su"];
+  const hourKey = [
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "11",
+    "12",
+    "13",
+    "14",
+    "15",
+    "16",
+    "17",
+    "18",
+    "19",
+    "20",
+    "21",
+    "22",
+    "23",
+    "24",
+  ];
+
+  return type === "day" ? dayKey : hourKey;
+};
