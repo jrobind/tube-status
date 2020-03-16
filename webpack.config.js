2b30730da25cf46c80e4432004cf122736a7fc2b
jrobind <jrobind@outlook.com>
2020-03-16 19:12:41 +0000

    add webpack

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./client/src/scripts/index.js",
+  output: {
+    path: path.resolve(__dirname, "client/build"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
+  mode: "production",
+};
