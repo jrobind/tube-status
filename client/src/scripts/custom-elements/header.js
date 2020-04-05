a6bdc85de51952303960d998af4665d52eed8324
jrobind <jrobind@outlook.com>
2020-04-05 14:25:02 +0100

    add dropdown options to avatar when signed in

--- client/src/scripts/custom-elements/header.js
+++ client/src/scripts/custom-elements/header.js
@@ -1,5 +1,6 @@
 import {store} from "../utils/client-store.js";
 import {actions} from "../constants.js";
+import {handleTabFocus, create, createFocusTrap} from "../utils/helpers.js";
 const {subscribeToStore, getStore} = store;
 
 /**
@@ -8,6 +9,9 @@ const {subscribeToStore, getStore} = store;
  */
 const cssClass = {
   HIDE: "tube-status-hide",
+  DOWNLOAD: "tube-status-avatar__dropdown-download-btn",
+  HEADER_AVATAR: "tube-status-header__avatar-image",
+  DELETE: "tube-status-avatar__dropdown-delete-btn",
 };
 
 /**
@@ -15,6 +19,7 @@ const cssClass = {
  * @enum {string}
  */
 const cssSelector = {
+  HEADER_AVATAR_WRAPPER: ".tube-status-header__avatar",
   HEADER_AVATAR: ".tube-status-header__avatar-image",
   HEADER_PROFILE: ".tube-status-header__profile",
 };
@@ -32,10 +37,14 @@ export default class Header extends HTMLElement {
     super();
 
     /** @private {HTMLImageElement} */
-    this.avatarEl_;
+    this.avatarEl_ = this.querySelector(cssSelector.HEADER_AVATAR);
 
     /** @private {HTMLElement} */
-    this.profileEl_;
+    this.profileEl_ = this.querySelector(cssSelector.HEADER_PROFILE);
+
+    /** @private {HTMLElement} */
+    this.avatarWrapperEl_ = this.querySelector(
+      cssSelector.HEADER_AVATAR_WRAPPER);
   }
 
   /**
@@ -47,10 +56,38 @@ export default class Header extends HTMLElement {
       action: actions.AUTHENTICATION,
     });
 
-    this.avatarEl_ = this.querySelector(cssSelector.HEADER_AVATAR);
-    this.profileEl_ = this.querySelector(cssSelector.HEADER_PROFILE);
+
+    this.avatarWrapperEl_.addEventListener(
+      "click", this.toggleDropdown_.bind(this));
+    this.avatarWrapperEl_.addEventListener(
+      "keyup", this.handleKeyup_.bind(this));
+    this.avatarWrapperEl_.addEventListener(
+      "keypress", this.handleKeyPress_.bind(this));
 
     this.updateAvatar_();
+  }
+
+  /**
+   * Handler for a tab keyup event.
+   * @param {KeyboardEvent} e
+   * @private
+   */
+  handleKeyup_(e) {
+    e.stopImmediatePropagation();
+    e.which === 9 && handleTabFocus(e.target);
+  }
+
+  /**
+   * Handler for a enter keypress event.
+   * @param {KeyboardEvent} e
+   * @private
+   */
+  handleKeyPress_(e) {
+    e.stopImmediatePropagation();
+
+    if (e.target.tagName !== "BUTTON") {
+      e.which === 13 && this.toggleDropdown_(e);
+    }
   }
 
   /**
@@ -63,5 +100,86 @@ export default class Header extends HTMLElement {
     this.avatarEl_.src = (signedIn) ?
       avatar :
       AVATAR_IMG_PATH;
+  }
+
+  /**
+   * Removes existing markup from avatar dropdown.
+   * @private
+   */
+  removeContent_() {
+    const children = Array.from(this.avatarWrapperEl_.children);
+
+    children.forEach((el) => {
+      if (!el.classList.contains(cssClass.HEADER_AVATAR)) {
+        this.avatarWrapperEl_.removeChild(el);
+      }
+    });
+
+    this.avatarEl_.removeAttribute("tabindex");
+  }
+
+  /**
+   * Handles user data deletion request.
+   * @private
+   */
+  handleDeleteRequest_() {
+  }
+
+  /**
+   * Handles user data download request.
+   * @private
+   */
+  handleDownloadRequest_() {
+  }
+
+  /**
+   * Handles toggling of the avatar dropdown once a user is signed in.
+   * @param {KeyboardEvent|Event} e
+   * @private
+   */
+  toggleDropdown_(e) {
+    const {target, target: {tagName}} = e;
+
+    if (tagName === "BUTTON") {
+      const downloadBtn = target.classList.contains(cssClass.DOWNLOAD);
+
+      downloadBtn ?
+        this.handleDownloadRequest_() :
+        this.handleDeleteRequest_();
+
+      return;
+    }
+
+    const {userProfile: {signedIn}} = getStore();
+    const contentExists = this.querySelector(
+      `.${cssClass.DOWNLOAD}`);
+
+    if (signedIn) {
+      if (contentExists) {
+        this.removeContent_();
+        return;
+      }
+      const downloadBtn = create("button", {
+        classname: cssClass.DOWNLOAD,
+        copy: "download data",
+        event: {type: "keyup", fn: handleTabFocus},
+        data: {name: "tabindex", value: "0"},
+      });
+      const deleteBtn = create("button", {
+        classname: cssClass.DELETE,
+        copy: "delete data",
+        event: {type: "keyup", fn: handleTabFocus},
+        data: {name: "tabindex", value: "0"},
+      });
+
+      this.avatarWrapperEl_.appendChild(downloadBtn);
+      this.avatarWrapperEl_.appendChild(deleteBtn);
+
+      this.avatarEl_.setAttribute("tabindex", "0");
+
+      // set focus and focus trap for dropdown elements
+      this.avatarWrapperEl_.focus();
+      createFocusTrap(this.avatarWrapperEl_);
+    }
   }
 }
