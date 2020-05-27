d96df645f0124270d8ccb3efad128c2ce99f9376
jrobind <jrobind@outlook.com>
2020-05-27 23:08:17 +0100

    finish (for now) authentication CE test suite

--- client/src/scripts/custom-elements/authentication/authentication.test.js
+++ client/src/scripts/custom-elements/authentication/authentication.test.js
@@ -1,17 +1,25 @@
 /* eslint-disable max-len */
 import Authentication from "./authentication.js";
+import waitForExpect from "wait-for-expect";
 import {store} from "../../utils/client-store.js";
+import {apiLogout} from "../../utils/api.js";
+import * as helpers from "../../utils/helpers.js";
 import {initialStore} from "../../utils/initial-store.js";
 import {actions} from "../../constants.js";
+jest.mock("../../utils/api.js");
 const {getStore, updateStore} = store;
-
 const testJWT = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjIxNzMyMTY5MzM3ODQ5NTE5NTg0MiIsImRpc3BsYXlOYW1lIjoiSm9obiBEb2UiLCJuYW1lIjp7ImZhbWlseU5hbWUiOiJEb2UiLCJnaXZlbk5hbWUiOiJKb2huIn0sInBob3RvcyI6W3sidmFsdWUiOiJodHRwczovL3Rlc3QtcGhvdG8ifV0sInByb3ZpZGVyIjoiZ29vZ2xlIiwiX3JhdyI6IntcbiAgXCJzdWJcIjogXCIxMDczMjE2OTQ0Nzg0OTUxOTU4NDFcIixcbiAgXCJuYW1lXCI6IFwiSm9obiBEb2VcIixcbiAgXCJnaXZlbl9uYW1lXCI6IFwiSm9oblwiLFxuICBcImZhbWlseV9uYW1lXCI6IFwiRG9lXCIsXG4gIFwicGljdHVyZVwiOiBcImh0dHBzOi8vdGVzdC1waG90b1wiLFxuICBcImxvY2FsZVwiOiBcImVuLUdCXCJcbn0iLCJfanNvbiI6eyJzdWIiOiIyMTczMjE2OTMzNzg0OTUxOTU4NDIiLCJuYW1lIjoiSm9obiBEb2UiLCJnaXZlbl9uYW1lIjoiSm9obiIsImZhbWlseV9uYW1lIjoiRG9lIiwicGljdHVyZSI6Imh0dHBzOi8vdGVzdC1waG90byIsImxvY2FsZSI6ImVuLUdCIn0sImlhdCI6MTU5MDQwOTgwMX0.5J68vM_RrnHo_6AgjhgjfFT6POQ2vwWzbIUDVDPqgwA";
+
+// spies
+const spyHandleJWT_ = jest.spyOn(Authentication.prototype, "handleJWT_");
+const spyHandleAuth_ = jest.spyOn(Authentication.prototype, "handleAuth_");
+const spyHandleError_ = jest.spyOn(Authentication.prototype, "handleError_").mockImplementation();
+const spyHandleTabFocus = jest.spyOn(helpers, "handleTabFocus");
 
 window.customElements.define("tube-status-authentication", Authentication);
 localStorage.setItem("JWT", testJWT);
 
-const spyHandleJWT_ = jest.spyOn(Authentication.prototype, "handleJWT_");
-
+// jsdom does not handle manipulation of window.location so we need to handle this
 delete window.location;
 window.location = {
   replace: jest.fn(),
@@ -70,12 +78,55 @@ describe("authentication element", () => {
     expect(authenticationEl.getAttribute("auth-path")).toBe("Sign in");
   });
 
-  it("Handles the user sign in process", () => {
+  it("Handles a successful user login", async () => {
     const authenticationEl = document.querySelector(".tube-status-authentication");
 
     authenticationEl.handleSignIn_();
 
     expect(getStore().loadingState.state).toBeTruthy;
-    expect(authenticationEl.classList.contains("tube-status-hide")).toBeTruthy;
+    expect(authenticationEl.classList.contains("tube-status-hide")).toBe(true);
+  });
+
+  it("Handles an unsuccessful user logout proccess", async () => {
+    const authenticationEl = document.querySelector(".tube-status-authentication");
+    apiLogout.mockImplementation(() => 500);
+
+    authenticationEl.handleLogout_();
+
+    await waitForExpect(() => {
+      expect(spyHandleError_).toHaveBeenCalledTimes(1);
+      expect(spyHandleError_).toHaveBeenCalledWith(500);
+    });
+  });
+
+  it("Handles a successful user logout process", async () => {
+    const authenticationEl = document.querySelector(".tube-status-authentication");
+    apiLogout.mockImplementation(() => 200);
+
+    authenticationEl.handleLogout_();
+
+    await waitForExpect(() => {
+      expect(localStorage.getItem("JWT")).toBe(null);
+    });
+  });
+
+  it("Handles a keyup event", async () => {
+    const authenticationEl = document.querySelector(".tube-status-authentication");
+    const event = new KeyboardEvent("keyup", {which: 9});
+
+    authenticationEl.dispatchEvent(event);
+
+    expect(spyHandleTabFocus).toHaveBeenCalledTimes(1);
+    expect(spyHandleTabFocus).toHaveBeenCalledWith(authenticationEl);
+  });
+
+  it("Handles a keypress event", async () => {
+    const authenticationEl = document.querySelector(".tube-status-authentication");
+    const event = new KeyboardEvent("keypress", {which: 13});
+
+    authenticationEl.dispatchEvent(event);
+
+    expect(spyHandleAuth_).toHaveBeenCalledTimes(1);
+    expect(spyHandleAuth_).toHaveBeenCalledWith(event);
   });
 });
