62adb72fd7c773c37bba80637fe5ed2b7b6f9b60
jrobind <jrobind@outlook.com>
2020-06-10 18:23:32 +0100

    add initial loading CE tests

---
+++ client/src/scripts/custom-elements/loading/loading.test.js
@@ -0,0 +1,30 @@
+/* eslint-disable max-len */
+import Loading from "./loading.js";
+import {store} from "../../utils/client-store.js";
+import * as helpers from "../../utils/helpers.js";
+import {initialStore} from "../../utils/initial-store.js";
+import {actions} from "../../constants.js";
+const {updateStore} = store;
+
+
+window.customElements.define("tube-status-loading", Loading);
+
+/**
+ * Tests
+ */
+describe("Loading element", () => {
+  beforeEach(() => {
+    document.body.innerHTML = `
+      <tube-status-loading class="tube-status-loading"></tube-status-loading>`;
+  });
+
+  afterEach(() => {
+    document.body.innerHTML = "";
+  });
+
+  it("Instantiates without error", () => {
+    const loadingElement = document.querySelector(".tube-status-loading");
+
+    expect(loadingElement.nodeType).toBe(1);
+  });
+});
