f070f5e56a10cb1d698cb784ffdf5a1960afa255
James Robinson <jrobind@outlook.com>
2020-08-02 14:02:46 +0100

    update webpack config to handle font files

--- webpack.config.js
+++ webpack.config.js
@@ -25,6 +25,18 @@ module.exports = {
           loader: "babel-loader",
         },
       },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'fonts/'
+            }
+          }
+        ]
+      },
     ],
   },
   plugins: [
